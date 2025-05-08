98c80cc5e02490324067fd5c69feafeeb76d9e97
lifelebisi <life.lebisi20@gmail.com>
2025-05-08 13:01:26 +0200

    added narbav

--- Accents/src/app/app.routes.ts
+++ Accents/src/app/app.routes.ts
@@ -1,6 +1,8 @@
+import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './pages/home/home.component';
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {path: '', title: 'Home', component: HomeComponent },
+  {path: 'nav', title: 'navbar', component: NavbarComponent }
 ];
