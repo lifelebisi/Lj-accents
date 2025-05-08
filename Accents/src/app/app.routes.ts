bf17f70aad6a25ac617f787d10752858508ac4ae
lifelebisi <life.lebisi20@gmail.com>
2025-05-08 12:05:53 +0200

    created angular

---
+++ Accents/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { HomeComponent } from './pages/home/home.component';
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {path: '', title: 'Home', component: HomeComponent },
+];
